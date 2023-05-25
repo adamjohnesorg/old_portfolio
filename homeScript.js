dfe92968f44d79770a90f59c45d52ef665ca6bb2
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2023-05-25 15:21:52 -0500

    Add files via upload

---
+++ homeScript.js
@@ -0,0 +1,19 @@
+function toMyBackground()
+{
+    window.location="./background.html";
+}
+
+function toHomePage()
+{
+    window.location="./index.html";
+}
+
+function toContactMe()
+{
+    window.location="./contact.html";
+}
+
+function toProjects()
+{
+    window.location="./projects.html";
+}
